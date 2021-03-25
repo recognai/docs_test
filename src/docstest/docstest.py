1ede56fbc9888746ae28b14d75f3b07905ccf622
dcfidalgo <david@recogn.ai>
2021-03-25 23:50:44 +0100

    setup

---
+++ src/docstest/docstest.py
@@ -0,0 +1,27 @@
+"""This is a module doc string
+
+This module is all about testing the docs.
+"""
+from typing import Optional
+
+
+class DocsTest:
+    """This is a class doc string"""
+    def __init__(self, a: str, b: Optional[float] = None):
+        self.a = a
+        self.b = b
+
+    def method_a(self, aa: int) -> bool:
+        """Some method doc string
+
+        Parameters
+        ----------
+        aa : int
+            Just a parameter
+
+        Returns
+        -------
+        bool
+        """
+        print(self.a)
+        return True
